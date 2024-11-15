80b70958816c98e4b6cdf732ff52bcf93d941b67
unknown <nelsondong158@gmail.com>
2024-11-14 19:31:17 -0500

    format

--- apps/main/src/app/not-found.tsx
+++ apps/main/src/app/not-found.tsx
@@ -1,7 +1,5 @@
-import React from "react"
+import React from "react";
 
 export default function NotFound(): JSX.Element {
-    return (
-        <div />
-    )
+  return <div />;
 }
--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,5 +1,6 @@
 import { type ReactNode } from "react";
 import React from "react";
+
 export function Card({
   title,
   children,
