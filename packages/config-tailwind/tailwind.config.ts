de72a97b33c876c56cc20007ec69611ee19ea523
unknown <nelsondong158@gmail.com>
2024-11-18 00:14:13 -0500

    example configs

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { colors, screens } from "./theme-tokens";
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, "content"> = {
@@ -9,6 +10,8 @@ const config: Omit<Config, "content"> = {
           "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
       },
     },
+    colors,
+    screens,
   },
   plugins: [],
 };
