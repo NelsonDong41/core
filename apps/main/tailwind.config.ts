952e1ad2cdc55443a6e0fd8b7bb9406ec91233ab
aaryan1203 <aaryanjain1203@gmail.com>
2024-11-21 21:12:10 -0500

    I hat efonts

--- apps/main/tailwind.config.ts
+++ apps/main/tailwind.config.ts
@@ -3,14 +3,9 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets" | "theme"> = {
-  content: ["./src/app/**/*.{tsx, ts}"],
+const config: Pick<Config, "content" | "presets"> = {
+  content: ["./src/app/**/*.tsx"],
   presets: [sharedConfig],
-  theme: {
-    fontFamily: {
-      "big-shoulder-display": ["Big Shoulder Display", "sans-serif"],
-    },
-  },
 };
 
 export default config;
