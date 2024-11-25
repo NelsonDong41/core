e923cfda587751d3c9e31ff9c38719e6a4cac01c
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2024-11-25 11:56:00 -0500

    added arrow props interface and function with onClick

--- packages/ui/src/arrowButton.tsx
+++ packages/ui/src/arrowButton.tsx
@@ -1,1 +1,15 @@
 "use client";
+
+import React from "react";
+
+interface ArrowButtonProps {
+  onClick: () => void;
+}
+
+export default function ArrowButton({ onClick }: ArrowButtonProps) {
+  return (
+    <>
+      <button onClick={onClick}> </button>
+    </>
+  );
+}
