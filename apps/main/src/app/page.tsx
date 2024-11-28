b7e974d737563d5b523c98326ba291609a1a20d0
NelsonDong41 <68518070+NelsonDong41@users.noreply.github.com>
2024-11-28 22:45:51 +0000

    example importing new font

--- apps/main/src/app/page.tsx
+++ apps/main/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center min-h-screen">
       <Stats />
+      <div className="font-weird-font">test</div>
     </main>
   );
 }
--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -9,6 +9,10 @@ const config: Omit<Config, "content"> = {
         "glow-conic":
           "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
       },
+      fontFamily: {
+        "big-shoulder-display": ['"Big Shoulder Display"', "cursive"],
+        "weird-font": ['"Eater"', "cursive"],
+      },
     },
     colors,
     screens,
