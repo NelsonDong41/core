1de15a82272c238357e1692c1febba3f9763df2c
aaryan1203 <aaryanjain1203@gmail.com>
2024-11-30 18:40:10 -0500

    Added fonts

--- packages/ui/src/StreetSign.tsx
+++ packages/ui/src/StreetSign.tsx
@@ -13,7 +13,7 @@ export default function StreetSign({
     <div className="flex justify-center items-center">
       <div className="relative inline-flex items-center bg-[#02877F] text-white border-2 border-white rounded-lg px-4 py-2 transform -rotate-2">
         <p className="text-lg font-big-shoulder-display">{streetName}</p>
-        <p className="text-sm ml-2 mb-1">{suffix}</p>
+        <p className="text-sm font-big-shoulder-display ml-2 mb-1">{suffix}</p>
       </div>
     </div>
   );
