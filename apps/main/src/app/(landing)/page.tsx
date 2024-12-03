43162d1bf18193eb6857b35e326f2ddfc8b5fa88
Alina <55455721+agonzalez22@users.noreply.github.com>
2024-12-02 21:15:19 -0500

    rm testing

--- apps/main/src/app/(landing)/page.tsx
+++ apps/main/src/app/(landing)/page.tsx
@@ -1,18 +1,13 @@
 import { Section } from "@repo/ui";
 import { Stats } from "./Sections";
-import { FAQDropdown } from "@repo/ui";
 import { Footer, NavBar } from "../lib/Components";
 import React from "react";
 
 export default function Page(): JSX.Element {
-  const q = "Question?";
-  const a =
-    "ANswejoidfjlsdkjfoweidjlksdfjklsdjflskjhfgksjdhglskdjfghslkdjfghslkdfjghskldjfghskldjfghsdkljfghsdlkfg";
   return (
     <main className="flex flex-col items-center min-h-screen">
       <NavBar />
       <Stats />
-      <FAQDropdown dropdownQuestion={q} dropdownAnswer={a} />
       <Section
         name="about"
         background={<>about</>}
